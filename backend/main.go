ca6d4f4fa4be5d6a95b2d044e2217044fa9663d6
f <medlfarssi10@gmail.com>
2025-04-04 00:42:30 +0000

    main code

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,10 @@
 package main
 
 func main()  {
+	database.DatabaseExecution()
+	defer database.CloseDatabase()
+	router.WebRouter()
+	router.ApiRouter()
 	fmt.Println("Server is running on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
