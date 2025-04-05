83546e266364003df97b332e8ceffb989bfbb13c
lfarssi <medlfarssi10@gmail.com>
2025-04-05 15:29:56 +0000

    parse file controller

--- backend/controllers/homeController.go
+++ backend/controllers/homeController.go
@@ -1,5 +1,35 @@
 package controllers
 
+
+import (
+	"net/http"
+	"text/template"
+)
+
 func HomePage()  {
 	
+}
+
+
+
+func ParseFileController(w http.ResponseWriter, r *http.Request, filename string, data any) {
+	filepath := "./resources/views/" + filename + ".html"
+	components := []string{
+		"./resources/views/components/navbar.html",
+		"./resources/views/components/footer.html",
+		"./resources/views/components/menu.html",
+		"./resources/views/components/posts.html",
+		"./resources/views/components/displayPost.html",
+	}
+	allFiles := append([]string{filepath}, components...)
+	temp, err := template.ParseFiles(allFiles...)
+	if err != nil {
+		ErrorController(w, r, http.StatusInternalServerError, "Cannot Parse File")
+		return
+	}
+	err1 := temp.Execute(w, data)
+	if err1 != nil {
+		ErrorController(w, r, http.StatusInternalServerError, "Cannot Execute File")
+		return
+	}
 }
