74601ad091e41b383b70dac8b1a55e1247b9a4bf
yelasri <elasriyoussef604@gmail.com>
2025-04-17 21:26:36 +0100

    update register handler

--- backend/utils/validName.go
+++ backend/utils/validName.go
@@ -6,7 +6,7 @@ func IsValidName(UserName string) bool {
 	}
 
 	for _, ele := range UserName {
-		if ele == '_' {
+		if ele == '_' || ele == ' ' {
 			continue
 		}
 		if !(ele >= 'a' && ele <= 'z') && !(ele >= 'A' && ele <= 'Z') && !(ele >= '0' && ele <= '9') {
