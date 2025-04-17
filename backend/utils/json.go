732565d2c11f0698a170d61fd3da186ef7015e40
lfarssi <medlfarssi10@gmail.com>
2025-04-17 21:18:30 +0100

    update register handler

--- backend/utils/json.go
+++ backend/utils/json.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func ResponseJSON(w http.ResponseWriter, status int, message map[string]any) {
+func ResponseJSON(w http.ResponseWriter, status int, message any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(message)
