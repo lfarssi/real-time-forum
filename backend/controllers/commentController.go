a6aa456004de9b8c41f02f8a9d16515fce4c7041
yelasri <elasriyoussef604@gmail.com>
2025-04-20 12:41:56 +0100

    get comments

--- backend/controllers/commentController.go
+++ backend/controllers/commentController.go
@@ -1,7 +1,32 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
 
-func GetCommnetsController(w http.ResponseController) {
+	"real_time_forum/backend/utils"
+)
 
+func GetCommnetsController(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		utils.ResponseJSON(w, http.StatusOK, map[string]any{
+			"message": "Method Not Allowed",
+			"status":  http.StatusMethodNotAllowed,
+		})
+		return
+	}
+
+	// comments, err := models.GetCommnets()
+	// if err != nil {
+	// 	utils.ResponseJSON(w, http.StatusInternalServerError, map[string]any{
+	// 		"message": "Error Getting Comments",
+	// 		"status":  http.StatusInternalServerError,
+	// 	})
+	// 	return
+	// }
+
+	// utils.ResponseJSON(w, http.StatusOK, map[string]any{
+	// 	"message": "Comments retrieved successfully",
+	// 	"status":  http.StatusOK,
+	// 	"data":    comments,
+	// })
 }
