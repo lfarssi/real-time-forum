ec5a664273733ff96b5b9bf43e394c505949a5ef
lfarssi <medlfarssi10@gmail.com>
2025-04-20 14:58:38 +0100

    tkhmal

--- backend/models/validators.go
+++ backend/models/validators.go
@@ -5,9 +5,5 @@ import "real_time_forum/backend/database"
 func IsExistsCategory(categoryID int) bool {
 	var id int
 	err := database.DB.QueryRow("SELECT id FROM category WHERE id = ?", categoryID).Scan(&id)
-	if err != nil {
-		return false
-	}
-
-	return true
+	return err == nil
 }
