0c4352fcc74caea4e70b50e8eeb621bd8ea01ccd
yelasri <elasriyoussef604@gmail.com>
2025-04-20 11:35:11 +0100

    tekhmal

--- backend/models/user.go
+++ backend/models/user.go
@@ -8,7 +8,7 @@ import (
 
 func Register(username, email, firstName, lastName, gender, password , age string) (int, error) {
 	var ID int
-	err := database.DB.QueryRow(`INSERT INTO users (Email, UserName, First_Name, Last_Name, Age, Gender, Password, Created_At, Session, Expared_At) 
+	err := database.DB.QueryRow(`INSERT INTO users (email, username, firstName, lastName, age, gender, password, createdAt, session, expiredAt) 
 	VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING ID`,
 		email, username, firstName, lastName, age, gender, password, time.Now().UTC(), "", nil).Scan(&ID)
 	if err != nil {
