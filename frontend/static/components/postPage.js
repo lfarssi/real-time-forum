a3f7766c1482d0b74dc99e633a681dc6fb6c171b
lfarssi <medlfarssi10@gmail.com>
2025-04-21 17:39:40 +0100

    man3rf

--- frontend/static/components/postPage.js
+++ frontend/static/components/postPage.js
@@ -3,9 +3,8 @@ import { errorPage } from "./errorPage.js";
 export async function PostsPage() {
     const response = await fetch("/api/getPosts");
     const data= await response.json()
-      if (!data.data ) {
-        document.body.innerHTML= errorPage("No Post Available",404 )
-        return 
+      if (!data.data ) {        
+        return errorPage("No Post Available",404 )
        
     }
     console.log(data.data);
@@ -27,9 +26,27 @@ export async function PostsPage() {
 
     `
 }
+export function PostForm() {
+    return /*html*/`
+          <form id="postForm">
+              <h2>Register</h2>
+            <input type="text" name="title"     placeholder="title"    />
+            <span class="errPost" id="title"></span>
+            <input type="text" name="content"        placeholder="content"       />
+            <span class="errPost" id="content"></span>
+            
+            <button type="submit">Register</button>
+          </form>
+      `
+}
 
-export function AddPosts() {
-    const form = document.querySelector("#registerForm");
+async function GetCategories() {
+  const response = await fetch("/api/getCategory");
+    const data= await response.json()
+}
+
+ function AddPosts() {
+    const form = document.querySelector("#postForm");
     form.addEventListener("click",async e=>{
         e.preventDefault()
         const formData = Object.fromEntries(new FormData(form).entries());
