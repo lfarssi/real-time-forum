d85c38759549298c068cb0df3188dcebccd26e47
lfarssi <medlfarssi10@gmail.com>
2025-04-25 17:44:40 +0100

    routing rate limit

--- backend/router/api.go
+++ backend/router/api.go
@@ -13,17 +13,17 @@ func ApiRouter() {
 	http.HandleFunc("/api/logout", controllers.LogoutController)
 	http.HandleFunc("/api/isLogged", middleware.IsLogged)
 
-	http.HandleFunc("/api/addPost", middleware.Authorization(http.HandlerFunc(controllers.AddPostController)))
+	http.HandleFunc("/api/addPost", middleware.Authorization(middleware.RateLimit(http.HandlerFunc(controllers.AddPostController))))
 	http.HandleFunc("/api/getPosts", middleware.Authorization(http.HandlerFunc(controllers.GetPostController)))
 	http.HandleFunc("/api/getLikedPosts", middleware.Authorization(http.HandlerFunc(controllers.GetLikedPostController)))
 	http.HandleFunc("/api/getCreatedPosts", middleware.Authorization(http.HandlerFunc(controllers.GetCreatedPostController)))
 	http.HandleFunc("/api/getPostsByCategory", middleware.Authorization(http.HandlerFunc(controllers.GetPostByCategoryController)))
 
-	http.HandleFunc("/api/addComment", middleware.Authorization(http.HandlerFunc(controllers.AddCommentController)))
+	http.HandleFunc("/api/addComment", middleware.Authorization(middleware.RateLimit(http.HandlerFunc(controllers.AddCommentController))))
 	http.HandleFunc("/api/getComments", middleware.Authorization(http.HandlerFunc(controllers.GetCommnetsController)))
 	http.HandleFunc("/api/getCategories", middleware.Authorization(http.HandlerFunc(controllers.CategoryController)))
 
-	http.HandleFunc("/api/addLike", middleware.Authorization(http.HandlerFunc(controllers.ReactPostController)))
+	http.HandleFunc("/api/addLike", middleware.Authorization(middleware.RateLimit(http.HandlerFunc(controllers.ReactPostController))))
 
 	http.HandleFunc("/api/getFriends", middleware.Authorization(http.HandlerFunc(controllers.FriendsController)))
 }
