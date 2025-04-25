a842afc039c386d503f88d10db8738fc965d8d84
lfarssi <medlfarssi10@gmail.com>
2025-04-25 11:17:56 +0100

    model message

--- backend/models/message.go
+++ backend/models/message.go
@@ -1,6 +1,33 @@
 package models
 
-func GetMessage(sender int, receiver int)([]Message ,error)  {
-	
-	
+import "real_time_forum/backend/database"
+
+func GetMessage(sender int, receiver int)([]*Message ,error)  {
+	query := `SELECT m.id,u.username, m.content , m.sentAt, m.status
+	FROM messages m 
+	INNER JOIN users u 
+	ON u.id=m.receiverID
+	WHERE senderID=? AND receiverID=?
+	`
+	rows, err := database.DB.Query(query, sender, receiver) 
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var messages []*Message
+	for rows.Next() {
+		var msg Message
+		err := rows.Scan(&msg.ID, &msg.Content, &msg.SentAt, &msg.Status, &msg.Username)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, &msg)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return messages, nil	
 }
