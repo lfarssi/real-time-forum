571bd9dd9112295e1a01b4787a0b0b10baf0ec2d
lfarssi <medlfarssi10@gmail.com>
2025-04-26 13:39:41 +0100

    update style friends

--- frontend/static/js/friends.js
+++ frontend/static/js/friends.js
@@ -4,9 +4,9 @@ export async function FriendsPage() {
     let friends = data.data.map(friend => {
         let gender
         if (friend.gender === "male") {
-            gender = '<i class="fa-solid fa-user male"></i>'
+            gender = '<i class="fa-solid fa-user online"></i>'
         } else {
-            gender = '<i class="fa-solid fa-user female"></i>'
+            gender = '<i class="fa-solid fa-user offline"></i>'
         }
         return /*html*/`
             <li>${gender} <span>${friend.firstName} ${friend.lastName}</span></li>
@@ -15,4 +15,14 @@ export async function FriendsPage() {
     return /*html*/`
         ${friends.join('')}
     `
+}
+
+export function chatFriend() {
+    let ul = document.querySelector('.friends ul')
+    ul.addEventListener('click', (e) => {
+        let friend = e.target.closest("li")
+        if (friend) {
+            console.log(friend)
+        }
+    })
 }
